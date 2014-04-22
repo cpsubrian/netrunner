a64432c5601f516b1f13e560e288931250e5760c
Brian Link <cpsubrian@gmail.com>
2014-04-21 22:18:12 -0700

    Correct card aspect ratio for single containers.

--- public/js/mixins/board.js
+++ public/js/mixins/board.js
@@ -1,12 +1,22 @@
-var $ = require('jquery');
+var $ = require('jquery')
+  , math = require('lib/math');
 
 var board = module.exports = {
   ui: {
     playmat: '.playmat',
     hud: '.hud',
-    singles: '.single'
+    singles: '.single',
+    zones: '.zone'
   },
   onRender: function () {
+    var cardHeight = $(window).height() / 5.5
+      , cardWidth = math.cardSize(null, cardHeight);
 
+    this.ui.singles.css({
+      width: cardWidth + 'px',
+      height: cardHeight + 'px'
+    });
+
+    this.ui.zones.height(cardHeight);
   }
 };

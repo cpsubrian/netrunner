a144c310e7cd7ac25ed264c40629e31fdb066a2f
Brian Link <cpsubrian@gmail.com>
2014-05-05 20:16:35 -0700

    Fixes image urls.

--- build/mixins/cards/card.js
+++ build/mixins/cards/card.js
@@ -1,26 +1,26 @@
 define("mixins/cards/card", function (require, exports, module) {
   var app = require('app');
-  
+
   /**
    * Card Mixin
    */
   module.exports = {
     className: 'card',
     template: require('hbs!card'),
-  
+
     events: {
       'mouseenter': 'onMouseEnter',
       'mouseleave': 'onMouseLeave'
     },
-  
+
     ui: {
       img: 'img'
     },
-  
+
     initialize: function (options) {
       var self = this;
       this.faceDown = (this.model.get('faceDown') || options.faceDown) || false;
-  
+
       // Preload images.
       setTimeout(function () {
         var small = new Image();
@@ -29,25 +29,25 @@ define("mixins/cards/card", function (require, exports, module) {
         large.src = 'http://netrunnerdb.com/' + self.model.get('largeimagesrc');
       }, 1);
     },
-  
+
     templateHelpers: function () {
       var data = {};
       if (this.faceDown) {
-        data.img = '/images/cards/misc/card-back-' + this.model.get('side_code') + '.jpg';
+        data.img = 'images/cards/misc/card-back-' + this.model.get('side_code') + '.jpg';
       }
       else {
         data.img = 'http://netrunnerdb.com/' + this.model.get('imagesrc');
       }
       return data;
     },
-  
+
     onMouseEnter: function () {
       app.vent.trigger('card:view:show', this);
     },
-  
+
     onMouseLeave: function () {
       app.vent.trigger('card:view:hide', this);
     }
-  
+
   };
 });
